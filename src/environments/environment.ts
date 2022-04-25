e2418d287dfe5990e08ff5e0fa4c1dafcbcd565a
emiliodeleonhdez <emiliodeleon.contact@gmail.com>
2022-04-25 10:51:03 -0500

    Setting prod env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   API_URL: "https://thebest-booking-api.herokuapp.com"
 };
 
