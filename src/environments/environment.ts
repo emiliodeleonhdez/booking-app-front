e7a23f0b4cde5ff6c4f48d9e977fdeebc8c9afbc
emiliodeleonhdez <emiliodeleon.contact@gmail.com>
2022-04-25 10:14:00 -0500

    Fixing API URL var to deploy in vercel

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  API_URL: "http://localhost:3001"
+  production: true,
+  API_URL: "https://thebest-booking-api.herokuapp.com"
 };
 
 /*
